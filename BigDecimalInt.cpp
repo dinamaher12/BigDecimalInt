19bce60f56dc55a207b0f611a8b1b3efb7ca4a30
abdelrhmannezzat <abdelrhmanakeel@gmail.com>
2022-10-14 13:57:17 +0200

    updated

--- BigDecimalInt.cpp
+++ BigDecimalInt.cpp
@@ -25,28 +25,38 @@ BigDecimalInt BigDecimalInt::operator-(BigDecimalInt anotherDec){
 
 
 // doesnot work with negative
-bool BigDecimalInt::operator>(BigDecimalInt anotherDec){
-
-    if(num.length() == anotherDec.num.length()){
-        for(int i = 0; i<num.length(); i++){
-
-            if(num[i] > anotherDec.num[i]){
-                return 1;
-
+bool BigDecimalInt::operator>(BigDecimalInt s){
+    string x = s.getstr();
+    if (x[0] == '-' && num[0] != '-'){
+        return true;
+    }
+    if (x[0] != '-' && num[0] == '-'){
+        return false;
+    }
+    int sz1 = num.size(),sz2 = x.size();
+    bool isPositive = (x[0] != '-');
+    if (x[0] == '-' || x[0] == '+'){
+        x.erase(x.begin());
+        sz2--;
+    }
+    if (num[0] == '-' || num[0] == '+'){
+        num.erase(num.begin());
+        sz1--;
+    }
+    if (sz1 == sz2){
+        for (int i = 0;i < sz1;++i){
+            if (x[i] > num[i]){
+                return (!isPositive);
             }
-            else if(num[i] < anotherDec.num[i]){
-                return 0;
-
+            else if(x[i] < num[i]){
+                return (isPositive);
             }
-
         }
+        return false;
     }
-
-    else if(num.length() > anotherDec.num.length()){
-        return 1;
-    }
-    else if(num.length() < anotherDec.num.length()){
-        return 0;
+    else
+    {
+        return (sz2 < sz1) && (!isPositive);
     }
 
 }
@@ -54,7 +64,7 @@ bool BigDecimalInt::operator>(BigDecimalInt anotherDec){
 
 
 // doesnot work with negative
-bool BigDecimalInt::operator<(BigDecimalInt anotherDec){
+bool BigDecimalInt::operator<(BigDecimalInt s){
     string x = s.getstr();
     if (x[0] == '-' && num[0] != '-'){ // if num is positive and x is negative return false
         return false;
