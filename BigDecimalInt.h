fb4af808a80f8c63d617f98dad54c470bc51e241
dinamaher12 <101825501+dinamaher12@users.noreply.github.com>
2022-10-16 15:42:02 +0200

    Add files via upload

---
+++ BigDecimalInt.h
@@ -0,0 +1,38 @@
+#ifndef P2_BIGDECIMALINT_H
+#define P2_BIGDECIMALINT_H
+#include<bits/stdc++.h>
+using namespace std;
+class BigDecimalInt{
+private:
+    string num;
+
+public:
+    // constructor
+    BigDecimalInt(string decStr); // rejects bad inputs
+
+
+    BigDecimalInt(int decInt);
+    string getstr();
+
+    BigDecimalInt operator+(BigDecimalInt anotherDec);
+
+    BigDecimalInt operator-(BigDecimalInt anotherDec);
+
+    bool operator<(BigDecimalInt anotherDec);
+
+    bool operator>(BigDecimalInt anotherDec);
+
+    bool operator==(BigDecimalInt anotherDec);
+
+    BigDecimalInt operator=(BigDecimalInt anotherDec); // lsa msh 3arfa
+
+    int size();
+
+    int sign();
+
+    friend ostream& operator<<(ostream& out, BigDecimalInt b);
+};
+
+
+
+#endif //P2_BIGDECIMALINT_H
