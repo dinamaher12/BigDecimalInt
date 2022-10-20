b490ff5178d01fe788bfb18754f31ea5d9b5dd61
abdelrhmannezzat <abdelrhmanakeel@gmail.com>
2022-10-20 15:47:32 +0200

    updated

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ int main(){
     cout << "num1 = " << num1 << endl;
     cout << "num2 = " << num2 << endl;
 
-
     //Next statement will print 236913578023691357802369135780
     cout << "num2 + num1 = " << num4 << endl;
     //Next statement will print -313456789011345678901134567890
